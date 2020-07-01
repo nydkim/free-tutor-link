e72ca437f6169f608c13b8e360b29f7cd88c558e
Nak Young Kim <nydkim@gmail.com>
2020-06-30 20:33:52 -0400

    last commit

--- server/controllers/availabilityController.js
+++ server/controllers/availabilityController.js
@@ -5,11 +5,10 @@ const availabilityController = {};
 availabilityController.getSubjectID = (req, res, next) => {
   // convert string subjectWord into ID, then pass to the next controller, getTutors
   const subjectWord = [req.params.subject];
-  // console.log('this is responseeeeeeeee>>>>', req.params);
   const sqlQuery = `SELECT _id FROM skills WHERE skill = $1`;
   db.query(sqlQuery, subjectWord)
     .then((data) => {
-      // console.log('DATA!!!', data.rows[0]._id);
+      console.log('DATA!!!', data.rows[0]._id);
       res.locals.skillid = data.rows[0]._id;
       return next();
     })
@@ -18,8 +17,7 @@ availabilityController.getSubjectID = (req, res, next) => {
         log:
           'availabiltyController.getSubjectID: ERROR: Error getting skill data from the database',
         message: {
-          err:
-            'availabiltyController.getSubjectID: ERROR: Check server logs for details',
+          err: 'availabiltyController.getSubjectID: ERROR: Check server logs for details',
         },
       });
     });
@@ -28,20 +26,18 @@ availabilityController.getSubjectID = (req, res, next) => {
 availabilityController.getTutors = (req, res, next) => {
   const subjectID = [res.locals.skillid];
   const sqlQuery = `SELECT name, skill, date, start_time AS start, end_time AS end , photo, linkedinprofile FROM tutors_skills INNER JOIN tutor_input_availability ON tutors_skills.tutor_id = tutor_input_availability.tutor_id LEFT JOIN tutors ON tutors._id = tutors_skills.tutor_id INNER JOIN skills ON skills._id = $1 AND skills._id = tutors_skills.skill_id;`;
-  // console.log('LOOK!', subjectID);
+  console.log('LOOK!', subjectID);
   db.query(sqlQuery, subjectID)
     .then((data) => {
-      // console.log('DATA!!!', data.rows);
+      console.log('DATA!!!', data.rows);
       res.locals.tutors = data.rows;
       return next();
     })
     .catch((err) => {
       return next({
-        log:
-          'availabiltyController.getTutor: ERROR: Error getting tutor data from the database',
+        log: 'availabiltyController.getTutor: ERROR: Error getting tutor data from the database',
         message: {
-          err:
-            'availabiltyController.getTutor: ERROR: Check server logs for details',
+          err: 'availabiltyController.getTutor: ERROR: Check server logs for details',
         },
       });
     });
